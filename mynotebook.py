8f2787734bcfb696f53677c5658d03641206792c
Victor Gan <victorgan@gmail.com>
2015-02-10 19:00:17 -0800

    a temporary notebook exported to python

---
+++ mynotebook.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# In[9]:
+
+a = 2
+
+
+# In[10]:
+
+a
+
+
+# # This is probably something I should do. 
+# ## Header 2
+# $ exp(i) $
+# 
+
+# In[13]:
+
+for i in range(8):
+    print i
+
+
+# ## This is a second header.
+
+# In[10]:
+
+from PIL import Image                                                                                
+img = Image.open('logo11w.png')
+img.show()
+
+
+# Example of a good iPython Notebook:
+# http://www.randalolson.com/wp-content/uploads/Evolution-of-Swarming-Experiment.pdf
